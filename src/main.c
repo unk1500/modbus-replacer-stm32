6b4d796f61d2a33b10fd82a598ce8b5957ec63d8
Sergey Savelev <snsavelev@istokmw.ru>
2026-08-08 21:05:05 +0300

    replace one led to four leds, add blinky for all of them, add directory for web files and gzip them

--- src/main.c
+++ src/main.c
@@ -1,24 +1,31 @@
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/net/http/server.h>
 #include <zephyr/net/http/service.h>
 
-#define LED_NODE DT_ALIAS(led_http)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+LOG_MODULE_REGISTER(app_main, LOG_LEVEL_ERR);
 
-static const uint8_t index_html[] =
-	"<!DOCTYPE html>"
-	"<html><head><meta charset=\"utf-8\"><title>STM32F4</title></head>"
-	"<body><h1>Hello from Zephyr on STM32F4Discovery!</h1></body></html>";
+static const struct gpio_dt_spec leds[] = {
+	GPIO_DT_SPEC_GET(DT_ALIAS(led_serial), gpios),
+	GPIO_DT_SPEC_GET(DT_ALIAS(led_status), gpios),
+	GPIO_DT_SPEC_GET(DT_ALIAS(led_alert), gpios),
+	GPIO_DT_SPEC_GET(DT_ALIAS(led_net), gpios),
+};
 
-static struct http_resource_detail_static index_html_resource_detail = {
+static uint8_t index_html_gz[] = {
+#include "index.html.gz.inc"
+};
+
+static struct http_resource_detail_static http_index_gz_resource_detail = {
 	.common = {
 		.type = HTTP_RESOURCE_TYPE_STATIC,
 		.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+		.content_encoding = "gzip",
 		.content_type = "text/html",
 	},
-	.static_data = index_html,
-	.static_data_len = sizeof(index_html) - 1,
+	.static_data = index_html_gz,
+	.static_data_len = sizeof(index_html_gz),
 };
 
 static uint16_t http_service_port = 80;
@@ -35,21 +42,39 @@ HTTP_SERVICE_DEFINE(
 );
 
 HTTP_RESOURCE_DEFINE(
-	index_html_resource,
+	http_index_gz_resource,
 	modbus_replacer_http_service,
 	"/",
-	&index_html_resource_detail
+	&http_index_gz_resource_detail
 );
 	
 
 int main(void)
-{
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
+{			
+	const size_t leds_count = ARRAY_SIZE(leds);
+	
+	for (size_t i = 0; i < leds_count; i++) {
+		if (!gpio_is_ready_dt(&leds[i])) {
+			LOG_ERR("GPIO device for led %d (%s) is not ready", (int)i, 
+            			leds[i].port != NULL ? leds[i].port->name : "NULL");
+			return -EIO;
+		}
+		
+		int ret = gpio_pin_configure_dt(&leds[i], GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			LOG_ERR("Failed to configure pin for led%d (err: %d)", (int)i, ret);
+			return ret;
+		}
 	}
-	gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 
 	http_server_start();
+	
+	while (1) {
+		for (size_t i = 0; i < leds_count; i++) {
+			gpio_pin_toggle_dt(&leds[i]);
+			k_msleep(200);
+		}
+	}
 
 	return 0;
 }
